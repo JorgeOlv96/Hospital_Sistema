5235839460ca96141293682b9982f94a264330d2
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-06-06 19:45:01 -0600

    Se agreggan estilos hacia Createsolicitud

--- src/screens/Solicitudes/Crearsolicitud.js
+++ src/screens/Solicitudes/Crearsolicitud.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../../Layout';
 import { Link, useHistory } from 'react-router-dom'; // Asegúrate de que useHistory esté importado
 
+
 function Solicitudes() {
 
   const [solicitudes, setSolicitudes] = useState([]);
@@ -31,144 +32,244 @@ function Solicitudes() {
 
   return (
     <Layout>
-      <div className="flex">
-        <div className="mr-4">
-          <label htmlFor="curp" className="block">Curp del paciente:</label>
-          <input type="text" id="curp" name="curp" className="border rounded-md px-2 py-1" />
+    <div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md">
+        <div className="mb-4">
+            <label htmlFor="curp" className="block font-semibold text-gray-700 mb-2">Curp del paciente:</label>
+            <input 
+                type="text" 
+                id="curp" 
+                name="curp" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            />
         </div>
-      </div>
 
-      <div className="flex mt-4">
-            <div className="mr-4">
-                <label htmlFor="apellidoPaterno" className="block">Apellido paterno:</label>
-                <input type="text" id="apellidoPaterno" name="apellidoPaterno" className="border rounded-md px-2 py-1" />
+        <div className="flex mt-4">
+            <div className="mr-4 w-full">
+                <label htmlFor="apellidoPaterno" className="block font-semibold text-gray-700 mb-2">Apellido paterno:</label>
+                <input 
+                    type="text" 
+                    id="apellidoPaterno" 
+                    name="apellidoPaterno" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                />
             </div>
 
-            <div className="mr-4">
-                <label htmlFor="apellidoMaterno" className="block">Apellido materno:</label>
-                <input type="text" id="apellidoMaterno" name="apellidoMaterno" className="border rounded-md px-2 py-1" />
+            <div className="mr-4 w-full">
+                <label htmlFor="apellidoMaterno" className="block font-semibold text-gray-700 mb-2">Apellido materno:</label>
+                <input 
+                    type="text" 
+                    id="apellidoMaterno" 
+                    name="apellidoMaterno" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                />
             </div>
             
-            <div className="mr-4">
-                <label htmlFor="nombres" className="block">Nombres:</label>
-                <input type="text" id="nombres" name="nombres" className="border rounded-md px-2 py-1" />
+            <div className="w-full">
+                <label htmlFor="nombres" className="block font-semibold text-gray-700 mb-2">Nombres:</label>
+                <input 
+                    type="text" 
+                    id="nombres" 
+                    name="nombres" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                />
             </div>
-      </div>
+        </div>
+    </div>
 
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="fechaNacimiento" className="block">Fecha de nacimiento:</label>
-          <input type="date" id="fechaNacimiento" name="fechaNacimiento" className="border rounded-md px-2 py-1" />
-      </div>
-      
-           <div className="mr-4">
-                <label htmlFor="sexo" className="block">Sexo:</label>
-                <select id="sexo" name="sexo" className="border rounded-md px-2 py-1">
+    <div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md mt-4">
+        <div className="flex mb-4">
+            <div className="mr-4 w-full">
+                <label htmlFor="fechaNacimiento" className="block font-semibold text-gray-700 mb-2">Fecha de nacimiento:</label>
+                <input 
+                    type="date" 
+                    id="fechaNacimiento" 
+                    name="fechaNacimiento" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                />
+            </div>
+            
+            <div className="w-full">
+                <label htmlFor="sexo" className="block font-semibold text-gray-700 mb-2">Sexo:</label>
+                <select 
+                    id="sexo" 
+                    name="sexo" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                >
                     <option value="masculino">Masculino</option>
                     <option value="femenino">Femenino</option>
                 </select>
             </div>
-       </div>
-
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="tipoConsulta" className="block">Tipo de consulta:</label>
-          <select id="tipoConsulta" name="tipoConsulta" className="border rounded-md px-2 py-1">
-            <option value="consultaExterna">Consulta Externa</option>
-            <option value="urgencias">Urgencias</option>
-            <option value="cirugia">Cirugía</option>
-          </select>
         </div>
-        
-           <div>
-            <label htmlFor="tipoIntervencion" className="block">Tipo de intervención:</label>
-              <select id="tipoIntervencion" name="tipoIntervencion" className="border rounded-md px-2 py-1">
-                <option value="ninguna">Ninguna</option>
-                <option value="cirugiaMenor">Cirugía Menor</option>
-                <option value="cirugiaMayor">Cirugía Mayor</option>
-              </select>
+    </div>
+
+
+    <div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md mt-4">
+        <div className="flex mb-4">
+            <div className="mr-4 w-full">
+                <label htmlFor="tipoConsulta" className="block font-semibold text-gray-700 mb-2">Tipo de consulta:</label>
+                <select 
+                    id="tipoConsulta" 
+                    name="tipoConsulta" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                >
+                    <option value="consultaExterna">Consulta Externa</option>
+                    <option value="urgencias">Urgencias</option>
+                    <option value="cirugia">Cirugía</option>
+                </select>
+            </div>
+            
+            <div className="w-full">
+                <label htmlFor="tipoIntervencion" className="block font-semibold text-gray-700 mb-2">Tipo de intervención:</label>
+                <select 
+                    id="tipoIntervencion" 
+                    name="tipoIntervencion" 
+                    className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+                >
+                    <option value="ninguna">Ninguna</option>
+                    <option value="cirugiaMenor">Cirugía Menor</option>
+                    <option value="cirugiaMayor">Cirugía Mayor</option>
+                </select>
             </div>
         </div>
+    </div>
+    <div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md mt-4">
+    <div className="flex mb-4">
+        <div className="mr-4 w-full">
+            <label htmlFor="fechaSolicitada" className="block font-semibold text-gray-700 mb-2">Fecha solicitada:</label>
+            <input 
+                type="date" 
+                id="fechaSolicitada" 
+                name="fechaSolicitada" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            />
+        </div>
+
+        <div className="mr-4 w-full">
+            <label htmlFor="horaSolicitada" className="block font-semibold text-gray-700 mb-2">Hora solicitada:</label>
+            <input 
+                type="time" 
+                id="horaSolicitada" 
+                name="horaSolicitada" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            />
+        </div>
+
+        <div className="mr-4 w-full">
+            <label htmlFor="tiempoEstimadoCirugia" className="block font-semibold text-gray-700 mb-2">Tiempo estimado de cirugía:</label>
+            <input 
+                type="time" 
+                id="tiempoEstimadoCirugia" 
+                name="tiempoEstimadoCirugia" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            />
+        </div>
+
+        <div className="w-full">
+            <label htmlFor="turnoSolicitado" className="block font-semibold text-gray-700 mb-2">Turno solicitado:</label>
+            <select 
+                id="turnoSolicitado" 
+                name="turnoSolicitado" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="Vespertino">Vespertino</option>
+                <option value="Matutino">Matutino</option>
+                <option value="Nocturno">Nocturno</option>
+                <option value="Especial">Especial</option>
+            </select>
+        </div>
+    </div>
+</div>
 
 
-        <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="fechaSolicitada" className="block">Fecha solicitada:</label>
-          <input type="date" id="fechaSolicitada" name="fechaSolicitada" className="border rounded-md px-2 py-1" />
+<div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md mt-4">
+    <div className="flex mb-4">
+        <div className="mr-4 w-full">
+            <label htmlFor="salaSolicitada" className="block font-semibold text-gray-700 mb-2">Sala solicitada:</label>
+            <select 
+                id="salaSolicitada" 
+                name="salaSolicitada" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="sala1">Sala 1</option>
+                <option value="sala2">Sala 2</option>
+                <option value="sala3">Sala 3</option>
+            </select>
         </div>
-        <div className="mr-4">
-          <label htmlFor="horaSolicitada" className="block">Hora solicitada:</label>
-          <input type="time" id="horaSolicitada" name="horaSolicitada" className="border rounded-md px-2 py-1" />
+        
+        <div className="w-full">
+            <label htmlFor="procedimientosPaciente" className="block font-semibold text-gray-700 mb-2">Procedimientos para el paciente:</label>
+            <select 
+                id="procedimientosPaciente" 
+                name="procedimientosPaciente" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="procedimiento1">Procedimiento 1</option>
+                <option value="procedimiento2">Procedimiento 2</option>
+                <option value="procedimiento3">Procedimiento 3</option>
+            </select>
         </div>
-        <div className="mr-4">
-          <label htmlFor="tiempoEstimadoCirugia" className="block">Tiempo estimado de cirugía:</label>
-          <input type="time" id="tiempoEstimadoCirugia" name="tiempoEstimadoCirugia" className="border rounded-md px-2 py-1" />
-        </div>
-        <div className="mr-4">
-          <label htmlFor="turnoSolicitado" className="block">Turno solicitado:</label>
-          <select id="turnoSolicitado" name="turnoSolicitado" className="border rounded-md px-2 py-1">
-            <option value="Vespertino">Vespertino</option>
-            <option value="Matutino">Matutino</option>
-            <option value="Nocturno">Nocturno</option>
-            <option value="Especial">Especial</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="salaSolicitada" className="block">Sala solicitada:</label>
-          <select id="salaSolicitada" name="salaSolicitada" className="border rounded-md px-2 py-1">
-            <option value="sala1">Sala 1</option>
-            <option value="sala2">Sala 2</option>
-            <option value="sala3">Sala 3</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="procedimientosPaciente" className="block">Procedimientos para el paciente:</label>
-          <select id="procedimientosPaciente" name="procedimientosPaciente" className="border rounded-md px-2 py-1">
-            <option value="procedimiento1">Procedimiento 1</option>
-            <option value="procedimiento2">Procedimiento 2</option>
-            <option value="procedimiento3">Procedimiento 3</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="cirujanoEncargado" className="block">Cirujano encargado:</label>
-          <select id="cirujanoEncargado" name="cirujanoEncargado" className="border rounded-md px-2 py-1">
-            <option value="cirujano1">Cirujano 1</option>
-            <option value="cirujano2">Cirujano 2</option>
-            <option value="cirujano3">Cirujano 3</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="requiereInsumos" className="block">Requiere insumos:</label>
-          <select id="requiereInsumos" name="requiereInsumos" className="border rounded-md px-2 py-1">
-            <option value="si">Sí</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <label htmlFor="especificarInsumos" className="block">Especifique los insumos:</label>
-          <select id="especificarInsumos" name="especificarInsumos" className="border rounded-md px-2 py-1">
-            <option value="insumo1">Insumo 1</option>
-            <option value="insumo2">Insumo 2</option>
-            <option value="insumo3">Insumo 3</option>
-          </select>
-        </div>
-      </div>
+    </div>
+</div>
 
 
-      <div className="flex mt-4">
-        <div className="mr-4">
-          <Link to="/solicitudes" className="btn btn-sm btn-secondary p-2 bg-[#001B58] text-white rounded-lg">Guardar</Link>
+<div className="flex flex-col p-4 bg-gray-100 rounded-lg shadow-md mt-4">
+    <div className="flex mb-4">
+        <div className="mr-4 w-full">
+            <label htmlFor="cirujanoEncargado" className="block font-semibold text-gray-700 mb-2">Cirujano encargado:</label>
+            <select 
+                id="cirujanoEncargado" 
+                name="cirujanoEncargado" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="cirujano1">Cirujano 1</option>
+                <option value="cirujano2">Cirujano 2</option>
+                <option value="cirujano3">Cirujano 3</option>
+            </select>
         </div>
-      </div>
+        
+        <div className="w-full">
+            <label htmlFor="requiereInsumos" className="block font-semibold text-gray-700 mb-2">Requiere insumos:</label>
+            <select 
+                id="requiereInsumos" 
+                name="requiereInsumos" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="si">Sí</option>
+                <option value="no">No</option>
+            </select>
+        </div>
+    </div>
+
+    <div className="flex mb-4">
+        <div className="mr-4 w-full">
+            <label htmlFor="especificarInsumos" className="block font-semibold text-gray-700 mb-2">Especifique los insumos:</label>
+            <select 
+                id="especificarInsumos" 
+                name="especificarInsumos" 
+                className="border border-gray-300 rounded-lg px-3 py-2 shadow-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" 
+            >
+                <option value="insumo1">Insumo 1</option>
+                <option value="insumo2">Insumo 2</option>
+                <option value="insumo3">Insumo 3</option>
+            </select>
+        </div>
+    </div>
+</div>
+
+
+
+<div className="flex mt-4">
+    <div className="mr-4">
+        <Link 
+            to="/solicitudes" 
+            className="inline-block bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out"
+        >
+            Guardar
+        </Link>
+    </div>
+</div>
+
+
 
     </Layout>
   );
