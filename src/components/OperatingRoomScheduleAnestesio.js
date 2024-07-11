4ac86d80af6e9631626b446a20952c60d9483abc
Gustavopvs1 <polen451@gmail.com>
2024-07-10 22:40:06 -0600

    Cambios a la vista de agenda y anestesiologos

--- src/components/OperatingRoomScheduleAnestesio.js
+++ src/components/OperatingRoomScheduleAnestesio.js
@@ -7,23 +7,23 @@ const OperatingRooms = ['A1', 'A2', 'T1', 'T2', '1', '2', '3', '4', '5', '6', 'E
 const OperatingRoomScheduleAnestesio = ({ date, anesthesiologists, onEventClick }) => {
   // Filtrar los anestesiólogos para la fecha seleccionada
   const filteredAnesthesiologists = anesthesiologists.filter(anesthesiologist =>
-    moment(anesthesiologist.start).isSame(date, 'day')
+    moment(anesthesiologist.start).isSame(date, 'day') || moment(anesthesiologist.end).isSame(date, 'day')
   );
 
   // Generar las filas y columnas para la tabla de horarios de quirófano
   const generateSchedule = () => {
-    const hours = Array.from({ length: 18 }, (_, i) => {
+    const hours = Array.from({ length: 24 }, (_, i) => {
       const hour = (i + 7) % 24;
       return `${String(hour).padStart(2, '0')}:00`;
     });
-    
+
     const schedule = OperatingRooms.map(room => {
       const roomAnesthesiologists = filteredAnesthesiologists.filter(anes =>
         anes.operatingRoom === room
       );
 
       const cells = hours.map((hour, index) => {
-        const startOfHour = moment(date).startOf('day').add(index, 'hours');
+        const startOfHour = moment(date).startOf('day').add(index + 7, 'hours');
         const endOfHour = moment(startOfHour).add(1, 'hour');
 
         const overlappingAnesthesiologists = roomAnesthesiologists.filter(anes =>
@@ -34,17 +34,16 @@ const OperatingRoomScheduleAnestesio = ({ date, anesthesiologists, onEventClick 
           return (
             <div key={hour} className="schedule-slot occupied">
               {overlappingAnesthesiologists.map((anesthesiologist, idx) => {
-                const startHour = moment(anesthesiologist.hora_inicio).hour();
-                const endHour = moment(anesthesiologist.hora_fin).hour();
-                const durationInHours = moment(anesthesiologist.hora_fin).diff(anesthesiologist.hora_inicio, 'hours', true);
+                const startMinute = moment(anesthesiologist.hora_inicio).diff(startOfHour, 'minutes');
+                const durationInMinutes = moment(anesthesiologist.hora_fin).diff(anesthesiologist.hora_inicio, 'minutes');
 
                 return (
                   <div
                     key={idx}
                     className="anesthesiologist-block"
                     style={{
-                      top: `${(startHour - index) * 100}%`,
-                      height: `${durationInHours * 100}%`,
+                      top: `${(startMinute / 60) * 100}%`,
+                      height: `${(durationInMinutes / 60) * 100}%`,
                     }}
                     onClick={() => onEventClick(anesthesiologist)}
                   >
