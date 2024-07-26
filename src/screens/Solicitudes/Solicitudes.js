a997b734b65e3d8232bcf469cff7b8ade54dbc6c
Gustavopvs1 <polen451@gmail.com>
2024-07-26 10:29:51 -0600

    Correcciones menores

--- src/screens/Solicitudes/Solicitudes.js
+++ src/screens/Solicitudes/Solicitudes.js
@@ -745,16 +745,15 @@ function Solicitudes() {
                           ></div>
                           <div className="mb-2 pl-3"> {/* Ajustado el padding left para acomodar la línea más ancha */}
                             <div className="flex justify-between">
-                              <p className="text-lg font-semibold">{solicitud.nombre_paciente}</p>
+                              <p className="text-lg font-semibold">{solicitud.nombre_paciente} {solicitud.ap_paterno} {solicitud.ap_materno}</p>
                               <p className="text-sm">{solicitud.sala}</p>
                             </div>
-                            <p className="text-sm text-gray-600">{solicitud.id_solicitud}</p>
+                            <p className="text-sm text-gray-600">{solicitud.folio}</p>
                             <p className="text-sm text-gray-600">{solicitud.nombre_especialidad}</p>
                             <div className="flex justify-between">
-                              <p className="text-sm text-gray-600">{solicitud.hora_inicio} - {solicitud.hora_fin}</p>
                               <p className="text-sm text-gray-600">{solicitud.turno}</p>
                             </div>
-                            <p className="text-sm text-gray-600">{solicitud.nombre_medico}</p>
+                            <p className="text-sm text-gray-600">{solicitud.nombre_cirujano}</p>
                             <p className="text-sm text-gray-600">{solicitud.insumos}</p>
                             <p className="text-sm text-gray-600">Estatus: {solicitud.estado_solicitud}</p>
                           </div>
