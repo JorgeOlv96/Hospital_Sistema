1707c243eb9a992ebe8c3fd8dda5888a6637b927
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-08-02 10:34:34 -0600

    Se realizan cambios sobre los campos de impresion y se corrige fecha

--- src/screens/Solicitudes/Solicitudes.js
+++ src/screens/Solicitudes/Solicitudes.js
@@ -273,7 +273,7 @@ function Solicitudes() {
                     <tr>
                       <td>${index + 1}</td>
                       <td>${appointment.folio || ""}</td>
-                      <td>${moment(appointment.hora_asignada, "HH:mm").format("LT")}</td>
+                      <td>${moment(appointment.hora_solicitada, "HH:mm").format("LT")}</td>
                       <td>${appointment.sala_quirofano || ""}</td>
                       <td>${appointment.nombre_paciente} ${appointment.ap_paterno} ${appointment.ap_materno}</td>
                       <td>${appointment.sexo ? (appointment.sexo === "Femenino" ? "F" : "M") : "No especificado"}</td>
