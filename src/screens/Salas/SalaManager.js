9e47e3b8e545f615f5878fd26f3641d7f27cfa29
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-08-07 11:12:46 -0600

    Se agrega tiempo inactivo

--- src/screens/Salas/SalaManager.js
+++ src/screens/Salas/SalaManager.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../Layout";
 import axios from "axios";
-import { Bar, Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { Bar, Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
 import "./SalaManager.css";
 
 // Registering Chart.js components
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 
 const SalaManager = () => {
   const [salas, setSalas] = useState([]);
@@ -38,35 +55,39 @@ const SalaManager = () => {
 
   // Prepare data for the bar chart
   const barChartData = {
-    labels: salas.map(sala => `Sala ${sala.nombre_sala}`),
+    labels: salas.map((sala) => `Sala ${sala.nombre_sala}`),
     datasets: [
       {
-        label: 'Estado de las Salas',
-        data: salas.map(sala => (sala.estado ? 1 : 0)), // 1 for available, 0 for not available
-        backgroundColor: salas.map(sala => (sala.estado ? '#4CAF50' : '#FF6F6F')), // Green for available, red for not available
-      }
+        label: "Estado de las Salas",
+        data: salas.map((sala) => (sala.estado ? 1 : 0)), // 1 for available, 0 for not available
+        backgroundColor: salas.map((sala) =>
+          sala.estado ? "#4CAF50" : "#FF6F6F"
+        ), // Green for available, red for not available
+      },
     ],
   };
 
   // Prepare data for the pie chart
-  const availableCount = salas.filter(sala => sala.estado).length;
+  const availableCount = salas.filter((sala) => sala.estado).length;
   const unavailableCount = salas.length - availableCount;
 
   const pieChartData = {
-    labels: ['Disponible', 'No Disponible'],
+    labels: ["Disponible", "No Disponible"],
     datasets: [
       {
-        label: 'Distribución de Salas',
+        label: "Distribución de Salas",
         data: [availableCount, unavailableCount],
-        backgroundColor: ['#4CAF50', '#FF6F6F'], // Green for available, red for not available
-      }
+        backgroundColor: ["#4CAF50", "#FF6F6F"], // Green for available, red for not available
+      },
     ],
   };
 
   return (
     <Layout>
       <div className="p-6">
-        <h1 className="text-2xl font-bold mb-6">Gestión de Salas de Quirófano</h1>
+        <h1 className="text-2xl font-bold mb-6">
+          Gestión de Salas de Quirófano
+        </h1>
         <div className="content-container">
           <div className="table-container">
             <table className="min-w-full divide-y divide-gray-200">
@@ -75,6 +96,8 @@ const SalaManager = () => {
                   <th className="text-center py-2">ID</th>
                   <th className="text-center py-2">Nombre</th>
                   <th className="text-center py-2">Estado</th>
+                  <th className="text-center py-2">Tiempo Inactivo</th>{" "}
+                  {/* Nueva columna */}
                   <th className="text-center py-2">Acciones</th>
                 </tr>
               </thead>
@@ -82,7 +105,9 @@ const SalaManager = () => {
                 {salas.map((sala) => (
                   <tr key={sala.id} className="border-b">
                     <td className="text-center py-2">{sala.id}</td>
-                    <td className="text-center py-2">Sala {sala.nombre_sala}</td>
+                    <td className="text-center py-2">
+                      Sala {sala.nombre_sala}
+                    </td>
                     <td className="text-center py-2">
                       <div className="status-container">
                         <span className="status-text">
@@ -90,6 +115,13 @@ const SalaManager = () => {
                         </span>
                       </div>
                     </td>
+                    <td className="text-center py-2">
+                      {/* Asegúrate de que 'sala.tiempo_inactivo' esté en el formato adecuado */}
+                      {sala.tiempo_inactivo
+                        ? `${sala.tiempo_inactivo} minutos`
+                        : "N/A"}
+                    </td>{" "}
+                    {/* Nueva celda */}
                     <td className="text-center py-2">
                       <div className="switch-container">
                         <label className="switch">
