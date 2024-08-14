16dbc2ebba15c098b75c568912527c7d64c87f7c
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-08-14 13:05:21 -0600

    corrigen estilos en consulta bitacora anestesiologo y enfermera

--- src/screens/Anestesio/Programaranestesiologo.js
+++ src/screens/Anestesio/Programaranestesiologo.js
@@ -294,6 +294,8 @@ const handleSort = (field) => {
 
   return (
     <Layout>
+
+    <ToastContainer position="bottom-right" />
       <div
         data-aos="fade-right"
         data-aos-duration="1000"
@@ -625,7 +627,7 @@ const handleSort = (field) => {
           </div>
 
           {/* Mostrar notificaciones */}
-          <ToastContainer position="bottom-right" />
+          
         </div>
       </div>
     </Layout>
