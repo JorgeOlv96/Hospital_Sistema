c467d146e33fd3a3b30d610ef9e8b1fdd26fa650
Gustavopvs1 <polen451@gmail.com>
2024-08-23 10:46:23 -0600

    Cambios en preprogramar fecha de solicitud formato

--- src/screens/Solicitudes/Programarsolicitud.js
+++ src/screens/Solicitudes/Programarsolicitud.js
@@ -839,7 +839,7 @@ function ProgramarSolicitud() {
                         {appointment.nombre_especialidad}
                       </td>
                       <td className="border px-4 py-2">
-                        {appointment.fecha_solicitada}
+                        {formatFechaSolicitada(appointment.fecha_solicitada)}
                       </td>
                       <td className="border px-4 py-2">
                         <div
